c00266efe60bafa0036e2dc9314fd9c4b5589882
Cam Carlson <camcarlsonreal@gmail.com>
2024-08-19 19:26:40 -0500

    Added db and working everything.

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'  # SQLite database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+
+# Create the database and tables
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
-@app.route('/about')
-def about():
-    return 'This is an about page'
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form.get('title')
+    new_task = Task(title=title)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:task_id>')
+def update_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        task.completed = not task.completed
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
